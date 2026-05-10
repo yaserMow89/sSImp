b3313b298648f1f52ff7f672406c0eaff6d7eb33
YaserMow89 <yaser.mowlawizadah@navvis.com>
2026-05-10 12:20:40 +0200

    Initiated chapter03

---
+++ Terraform/terraformUpAndRunning/chapter03/singleServer.tf
@@ -0,0 +1,168 @@
+provider "aws" {
+  region = "eu-central-1"
+}
+
+// Getting the vpc id
+data "aws_vpc" "default" {
+  default = true
+}
+
+// Getting the subnet id in the default vpc
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+  type        = number
+  default     = 8080
+}
+
+resource "aws_launch_template" "example" {
+  image_id      = "ami-0abe96a6773a37eb1"
+  instance_type = "t3.micro"
+  user_data = base64encode(<<-EOF
+              #!/bin/bash
+              echo "hello world!" > index.xhtml
+              nohup busybox httpd -f -p ${var.server_port} &
+              EOF
+  )
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_autoscaling_group" "example" {
+  launch_template {
+    id = aws_launch_template.example.id
+  }
+  max_size            = 10
+  min_size            = 2
+  vpc_zone_identifier = data.aws_subnets.default.ids
+  //
+  target_group_arns = [aws_lb_target_group.asg.arn]
+  health_check_type = "ELB"
+  tag {
+    key                 = "Name"
+    value               = "terraform-ags-example"
+    propagate_at_launch = true
+  }
+}
+
+resource "aws_lb_target_group" "asg" {
+  name     = "terraform-asg-example"
+  port     = var.server_port
+  protocol = "HTTP"
+  vpc_id   = data.aws_vpc.default.id
+
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200"
+    interval            = 15
+    timeout             = 3
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+}
+
+resource "aws_lb" "example" {
+  name               = "terraform-asg-example"
+  load_balancer_type = "application"
+  // Use all the subnets in the default VPC
+  subnets         = data.aws_subnets.default.ids
+  security_groups = [aws_security_group.alb.id]
+}
+
+output "alb" {
+  value       = aws_lb.example.dns_name
+  description = "PUBLIC DNS NAME"
+}
+
+resource "aws_alb_listener" "http" {
+  load_balancer_arn = aws_lb.example.arn
+  port              = 80
+  protocol          = "HTTP"
+  // Default resonse if the page doesn't exist
+  default_action {
+    type = "fixed-response"
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "404: page not found"
+      status_code  = 404
+    }
+  }
+}
+
+resource "aws_lb_listener_rule" "asg" {
+  listener_arn = aws_alb_listener.http.arn
+  priority     = 100
+  condition {
+    path_pattern {
+      values = ["*"]
+    }
+  }
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.asg.arn
+  }
+}
+
+resource "aws_security_group" "alb" {
+  name = "terraform-example-alb"
+  ingress = [
+    {
+      from_port        = 80
+      to_port          = 80
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+      prefix_list_ids  = []
+      security_groups  = []
+      self             = false
+      description      = "Simple ingress for the lb"
+    }
+  ]
+  egress = [
+    {
+      from_port        = 0
+      to_port          = 0
+      protocol         = "-1"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+      prefix_list_ids  = []
+      security_groups  = []
+      self             = false
+      description      = "Simple egress for the lb"
+    }
+  ]
+}
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+  ingress = [
+    {
+      description      = "web"
+      from_port        = var.server_port
+      to_port          = var.server_port
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+      prefix_list_ids  = []
+      security_groups  = []
+      self             = false
+    }
+  ]
+}
+
+/*
+  ____ _                 _               ___ _____
+ / ___| |__   __ _ _ __ | |_ ___ _ __   / _ \___ /
+| |   | '_ \ / _` | '_ \| __/ _ \ '__| | | | ||_ \
+| |___| | | | (_| | |_) | ||  __/ |    | |_| |__) |
+ \____|_| |_|\__,_| .__/ \__\___|_|     \___/____/
+                  |_|
+*/
